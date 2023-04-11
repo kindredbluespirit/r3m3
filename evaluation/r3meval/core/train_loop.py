a0bf616a035a31580df8f0adecd33ff951622fba
Sai Tarun Inaganti <46247049+onetruffle@users.noreply.github.com>
2023-04-11 02:22:07 -0500

    Update train_loop.py

--- evaluation/r3meval/core/train_loop.py
+++ evaluation/r3meval/core/train_loop.py
@@ -96,7 +96,8 @@ def bc_train_loop(job_data:dict) -> None:
 
     # Infers the location of the demos
     ## V2 is metaworld, V0 adroit, V3 kitchen
-    data_dir = '/iris/u/surajn/data/r3m/'
+    # data_dir = '/iris/u/surajn/data/r3m/'
+    data_dir = '/content/bc_data_clean/' # colab
     if "v2" in job_data['env_kwargs']['env_name']:
         demo_paths_loc = data_dir + 'final_paths_multiview_meta_200/' + job_data['camera'] + '/' + job_data['env_kwargs']['env_name'] + '.pickle'
     elif "v0" in job_data['env_kwargs']['env_name']:
